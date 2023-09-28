b6f88450fef433f351dad73f6265181f466a9aac
Maria Ap. Santos <138404683+mariaapbez@users.noreply.github.com>
2023-09-28 11:22:53 -0300

    atualização/resolução exercício fastAPI SQLAlchemy
    desenvolvimento do exercício fastAPI SQLAlchemy, análise dos arquivos baixados, teste da API, inserção, leitura, atualização, deleção de notas.

---
+++ exercicios_fastAPI_SQLAlchemy/app/schemas.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel
+
+
+class NoteBaseSchema(BaseModel):
+    id: str | None = None
+    title: str
+    content: str
+    category: str | None = None
+    published: bool = False
+    createdAt: datetime | None = None
+    updatedAt: datetime | None = None
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+
+
+class ListNoteResponse(BaseModel):
+    status: str
+    results: int
+    notes: List[NoteBaseSchema] = [
+        NoteBaseSchema(
+            id= None,
+            title= "Teste API",
+            content= "Teste API Desafio 3",
+            category= "Teste",
+            published= False,
+            createdAt= None,
+            updatedAt= None
+        )
+    ]
